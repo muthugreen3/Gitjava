5a0bf6cd33e50bc5a8c5c761c30718d3d5646194
muthugreen3 <muthugreen3@gmail.com>
2022-03-22 12:24:48 +0530

    muthuda

---
+++ src/com/comee/Vamame.java
@@ -0,0 +1,14 @@
+package com.comee;
+
+public class Vamame {
+
+	public static void main(String[] args) {
+	String name = "pasikuthu";
+	if (name=="pasikuthu") {
+		System.out.println("moditu thinnu");
+	}
+	else {
+		System.out.println("velaya paru");
+	}
+	}
+}
